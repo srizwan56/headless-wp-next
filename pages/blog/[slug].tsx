76b78674a46490232c4bf884270a79348c7cc5d7
Rizwan <srizwan56@gmail.com>
2025-12-03 00:36:51 +0530

    Fix slug page build error

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,18 +1,38 @@
+// pages/blog/[slug].tsx
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { fetchGraphQL } from '../../lib/graphql';
 
-const SLUGS_QUERY = `query { posts(first: 100){ nodes { slug } } }`;
-const POST_QUERY = `query Post($slug: ID!){ post(id: $slug, idType: SLUG) { title content date } }`;
+const SLUGS_QUERY = `
+  query {
+    posts(first: 100) {
+      nodes {
+        slug
+      }
+    }
+  }
+`;
+
+const POST_QUERY = `
+  query Post($slug: ID!) {
+    post(id: $slug, idType: SLUG) {
+      title
+      content
+      date
+    }
+  }
+`;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const data = await fetchGraphQL(SLUGS_QUERY);
-  const paths = data.posts.nodes.map((p: any) => ({ params: { slug: p.slug } }));
+  const paths =
+    data?.posts?.nodes?.map((p: any) => ({ params: { slug: p.slug } })) || [];
   return { paths, fallback: 'blocking' };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const data = await fetchGraphQL(POST_QUERY, { slug: params!.slug });
-  if (!data.post) return { notFound: true };
+  const slug = params?.slug as string;
+  const data = await fetchGraphQL(POST_QUERY, { slug });
+  if (!data?.post) return { notFound: true };
   return { props: { post: data.post }, revalidate: 60 };
 };
 
@@ -24,4 +44,3 @@ export default function Post({ post }: any) {
     </main>
   );
 }
-git branch
